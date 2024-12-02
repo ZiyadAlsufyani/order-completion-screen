89f38466af7df5ab5a62c31b323e91abb9b986e7
Ziyad Alsufyani <zeyadxxx333@gmail.com>
2024-12-02 19:38:17 +0300

    first commit

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,11 @@
+import OrderCompletionScreen from './components/OrderCompletionScreen'
+
+function App() {
+  return (
+    <>
+      <OrderCompletionScreen />
+    </>
+  )
+}
+
+export default App
---
+++ src/renderer/src/components/OrderCompletionScreen.jsx
@@ -0,0 +1,11 @@
+import StickyHeader from './StickyHeader'
+import OrderList from './OrderList'
+
+export default function OrderCompletionScreen() {
+  return (
+    <>
+      <StickyHeader title="Order Completion Screen" />
+      <OrderList btnColor="neutral" />
+    </>
+  )
+}
