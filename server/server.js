1fd5a8bedc85af63098e93481ff1b51f1ce04ae3
Ali <ali.fa.asiri@gmail.com>
2024-12-14 13:35:03 +0300

    Update domain_id in QSystem.xml and enhance server.js for order processing

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 
 const data = []; // Moved outside the function to ensure it is accessible
 
+const connector2 = new rti.Connector('OrderCompletionScreenDomainParticipantLibrary::MyPubParticipant', configFile);
+const output2 = connector2.getOutput('MyPublisher::MySquareWriter');
+
 const run = async () => {
   const connector = new rti.Connector('OrderCompletionScreenDomainParticipantLibrary::MySubParticipant', configFile);
   const input = connector.getInput('MySubscriber::MySquareReader');
@@ -48,31 +51,32 @@ app.post('/write', async (req, res) => {
   const { fromDevice, toDevice, orderNum } = req.body;
   console.log(req.body);
 
-  const connector = new rti.Connector('OrderCompletionScreenDomainParticipantLibrary::MyPubParticipant', configFile);
-  const output = connector.getOutput('MyPublisher::MySquareWriter');
 
   try {
-    console.log('Waiting for subscriptions...');
-    const waitTime = 5000; // Timeout in milliseconds
-    const hasSubscriptions = await output.waitForSubscriptions(waitTime);
 
-    if (!hasSubscriptions) {
-      throw new Error('No subscriptions found');
-    }
 
     console.log('Writing...');
-    output.instance.setString('fromDevice', fromDevice);
-    output.instance.setString('toDevice', toDevice);
-    output.instance.setNumber('orderNum', orderNum);
-    output.write();
+    output2.instance.setString('fromDevice', fromDevice);
+    output2.instance.setString('toDevice', toDevice);
+    output2.instance.setNumber('orderNum', orderNum);
+    output2.write();
 
     res.status(200).send('Data written successfully');
   } catch (err) {
     console.error('Error encountered:', err);
     res.status(500).send('Failed to write data: ' + err.message);
-  } finally {
-    connector.close();
-  }
+  } 
+});
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received: closing connector');
+  connector.close();
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing connector');
+  connector.close();
+  process.exit(0);
 });
 
 app.listen(5002, async () => {
